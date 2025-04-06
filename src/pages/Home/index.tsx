c1b53ffbb4e17743da60a62b8e5a6520127448ad
Jhony Freitas <jhony00._@hotmail.com>
2025-04-06 03:05:37 -0300

    feat: Init Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,188 @@
-import { Container } from "./styles";
+import { useEffect, useState } from "react";
+import { IoMdArrowDropleft } from "react-icons/io";
+import { IoMdArrowDropright } from "react-icons/io";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { IoMdArrowDropup } from "react-icons/io";
+import { RiEyeCloseFill } from "react-icons/ri";
+import { BsFillEyeFill } from "react-icons/bs";
+import { AccountItem, AccountList, Card, CardFooter, CardHeader, CardList, Container, CurrentAccount, Footer, IconCardContainer, InvoiceSpan, InvoiceValue, MainContent, MainDetails, Month, MonthContainer, Price, PriceContainer, TextCard, TextCardSpan, ToggleAccount } from "./styles";
 
 export function Home () {
+  const [monthIndex, setMonthIndex] = useState<number>(0);
+  const [accontToggle, setAccontToggle] = useState<boolean>(false);
+  const [selectedAccountId, setSelectedAccountId] = useState<number>(1);
+  const [showPrice, setShowPrice] = useState<boolean>(false);
+  const [cardIdInDetails, setCardIdInDetails] = useState<number | null>(null);
+
+  useEffect(() => {
+    const date = new Date()
+    const currentMonth = date.getMonth()
+    setMonthIndex(currentMonth)
+  }, []);
+
+  const months = [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Maio',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez',
+  ]
+
+  const accountList = [
+    { id: 1, name: 'Principal', value: '500,00'  },
+    { id: 2, name: 'Poupança', value: '400,00'  },
+    { id: 3, name: 'Investimentos', value: '300,99'  },
+  ]
+
+  function handlePrevMonth () {
+    if (!monthIndex) return setMonthIndex(11)
+    setMonthIndex(prev => prev - 1)
+  }
+
+  function handleNextMonth () {
+    if (monthIndex === 11) return setMonthIndex(0)
+    setMonthIndex(prev => prev + 1) 
+  }
+
+  function handleSelectAccount (id: number) {
+    setAccontToggle(false)
+    setSelectedAccountId(id)
+  }
+
+  function handleDetailsCard(id: number) {
+    if (cardIdInDetails === id) return setCardIdInDetails(null) 
+    setCardIdInDetails(id)
+  }
+
+  const cardList = [
+    { 
+      id: 1,
+      name: 'nuBank',
+      limit: '1000,00',
+      invoice: '1500,00',
+      color: '#820AD1',
+      closing: '26',
+      dueDate: '30',
+    },
+    { 
+      id: 2,
+      name: 'C6',
+      limit: '1200,00',
+      invoice: '1000,00',
+      color: '#121212',
+      closing: '24',
+      dueDate: '28',
+    },
+    { 
+      id: 3,
+      name: 'Mercado Pago',
+      limit: '2000,00',
+      invoice: '890,50',
+      color: '#009EE3',
+      closing: '20',
+      dueDate: '25',
+    },
+  ]
+
   return (
     <Container>
-      Hellow
+      <MainDetails>
+        <MonthContainer>
+          <IoMdArrowDropleft fill="#fff" size={26} onClick={handlePrevMonth}/>
+          <Month>{months[monthIndex]}</Month>
+          <IoMdArrowDropright fill="#fff" size={26} onClick={handleNextMonth}/>
+        </MonthContainer>
+
+        <ToggleAccount>
+          <CurrentAccount>
+            {accountList.find(({id}) => id === selectedAccountId)?.name}
+            { accontToggle
+              ? <IoMdArrowDropup size={22} onClick={() => setAccontToggle(false)} />
+              : <IoMdArrowDropdown size={22} onClick={() => setAccontToggle(true)} />
+            }
+          </CurrentAccount>
+
+          <AccountList $open={accontToggle}>
+            {accountList
+              .filter(({id}) => (id !== selectedAccountId))
+              .map(({id, name}, i ) => (
+                <AccountItem key={i} onClick={() => handleSelectAccount(id)}>
+                  {name}
+                </AccountItem>
+              ))}
+          </AccountList>
+        </ToggleAccount>
+
+        <PriceContainer>
+          <Price>
+            {'R$ '}
+            {showPrice 
+              ? accountList.find(({id}) => id === selectedAccountId)?.value 
+              : '.....'
+            }
+          </Price>
+
+          {showPrice
+            ? <BsFillEyeFill size={24} onClick={() => setShowPrice(false)} />
+            : <RiEyeCloseFill size={24} onClick={() => setShowPrice(true)} />
+          }
+        </PriceContainer>
+      </MainDetails>
+      
+      <MainContent>
+          <CardList>
+            {cardList.map(card => (
+              <Card 
+                key={card.id}
+                $color={card.color}
+                $details={cardIdInDetails === card.id}
+                onClick={() => handleDetailsCard(card.id)}
+              >
+                <CardHeader>
+                  <TextCard $name>{card.name}</TextCard>
+                  <TextCard $column>
+                    <TextCardSpan>limite disponível</TextCardSpan> {`R$ ${card.limit}`}
+                  </TextCard>
+
+                  { cardIdInDetails !== card.id && (
+                      <IconCardContainer>
+                        <IoMdArrowDropdown size={28} style={{margin: 'auto'}}/>
+                      </IconCardContainer>
+                    )
+                  }
+                </CardHeader>
+
+                <InvoiceValue>
+                  {`R$ ${card.invoice}`}
+                  <InvoiceSpan>
+                    fatura aberta
+                  </InvoiceSpan>
+                </InvoiceValue>
+
+                <CardFooter>
+                  <TextCard $column>
+                    {`dia ${card.closing}`}
+                    <TextCardSpan>fechamento</TextCardSpan>
+                  </TextCard>
+
+                  <TextCard $column>
+                    {`dia ${card.dueDate}`}
+                    <TextCardSpan>vencimento</TextCardSpan>
+                  </TextCard>
+                </CardFooter>
+              </Card>
+            ))}
+          </CardList>
+
+      </MainContent>
+      <Footer></Footer>
     </Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,161 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  width: 100%;
+`;
+
+export const MainDetails = styled.section`
+  display: flex;
+  flex-direction: column;
+  border-radius: 0  0 20px 20px;
+  background-color: #528da4;
+  padding: 15px;
+  min-height: 22vh;
+  width: 100%;
+`;
+
+export const MonthContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Month = styled.div`
+  color: #fff;
+  font-size: 14px;
+  text-align: center;
+  width: 40px;
+`;
+
+export const ToggleAccount = styled.div`
+  position: relative;
+`;
+
+export const CurrentAccount = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 17px;
+  margin: 30px 0 0 15px;
+  width: 130px;
+`;
+
+export const AccountList = styled.ul<{$open: boolean}>`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  transform: translateY(100%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2px;
+  background-color: aliceblue;
+  border-radius: 4px;
+  width: 140px;
+  padding: ${({ $open }) => $open ? '5px' : '0px'};
+  margin: 5px 0 0 10px;
+  transition: all 0.4s ease;
+  overflow: auto;
+  max-height: ${({ $open }) => $open ? '65px' : '0px' };
+`;
+
+export const AccountItem = styled.li`
+`;
+
+export const PriceContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  width: 100%;
+  padding: 10px 15px; 
+  margin-top: 12px;
+`;
+
+export const Price = styled.p`
+  font-size: 28px;
+  font-weight: 500;
+`;
+
+export const MainContent = styled.main`
+  flex: 1;
+  overflow: auto;
+  width: 100%;
+`;
+
+export const CardList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 15px;
+  padding: 10px 15px;
+  width: 100%;
+`;
+
+export const Card = styled.li<{$details: boolean; $color: string}>`
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ $color }) => $color};;
+  border-radius: 10px;
+  transition: all 0.4s ease;
+  padding: 12px;
+  overflow: hidden;
+  height: ${({ $details }) => $details ? '157px': '60px' };
+  width: 100%;
+`;
+
+export const CardHeader = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 14px;
+  width: 100%;
+`;
+
+export const IconCardContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 60%);
+`;
+
+export const TextCard = styled.p<{$column?: boolean; $name?: boolean}>`
+  display: flex;
+  font-size: ${({ $name }) => $name ? '18px' : '15px'};
+  flex-direction: ${({ $column }) => $column && 'column'};
+  gap: 2px;
+  color: #fff;
+`;
+
+export const TextCardSpan = styled.span`
+  font-size: 11px;
+`;
+
+export const InvoiceValue = styled.p`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  color: #fff;
+  font-size: 20px;
+`;
+
+export const InvoiceSpan = styled.span`
+  font-size: 12px;
+`;
+
+export const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 15px;
+`;
+
+export const Footer = styled.footer`
+  display: flex;
+  background-color: #528da4;
+  padding: 15px;
+  width: 100%;
+  min-height: 10vh;
 `;
