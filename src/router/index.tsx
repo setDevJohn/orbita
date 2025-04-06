533dfa3cb5df1280d7967947a3449e1b6f101bc5
Jhony Freitas <jhony00._@hotmail.com>
2025-04-06 03:06:14 -0300

    faet: Config Outlet schema

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from '@pages/Home';
+import { NaviGation } from '@pages/Navigation';
 
 export const AppRoutes = () => {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<NaviGation />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
