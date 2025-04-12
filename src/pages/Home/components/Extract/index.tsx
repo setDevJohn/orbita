ce81e475aa42f2f5536f52d14d2f04bdd189c22d
Jhony Freitas <jhony00._@hotmail.com>
2025-04-11 23:21:49 -0300

    feat: Create Extract page

--- src/pages/Home/components/Extract/index.tsx
+++ src/pages/Home/components/Extract/index.tsx
@@ -1,5 +1,157 @@
-export function ExtractComponent () {
+import { useState } from "react";
+import { SearchFilter } from "components/Filters/Search";
+import { DateFilter } from "components/Filters/Date";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { FaArrowTrendDown } from "react-icons/fa6";
+import { 
+  ButtonContainer,
+  ButtonFilter,
+  ExtractDate,
+  ExtractDesc,
+  ExtractItem,
+  ExtractItemContent,
+  ExtractList,  
+  FilterContainer,  
+  Line,
+  RecentExtract,
+  Title
+} from "./styles";
+
+export function ExtractComponent() {
+  const [typeListFilter, setTypeListFilter] = useState<string>('');
+
+  const extractDateList = [
+    { 
+      date: '01/04',
+      list: [
+        {
+          name: 'Mesada',
+          type: 'in',
+          price: 'R$ 25,99'
+        },
+        {
+          name: 'Pizzaria',
+          type: 'out',
+          price: 'R$ 45,90'
+        }
+      ]
+    },
+    { 
+      date: '02/04',
+      list: [
+        {
+          name: 'Mesada',
+          type: 'in',
+          price: 'R$ 25,99'
+        },
+        {
+          name: 'Pizzaria',
+          type: 'out',
+          price: 'R$ 45,90'
+        }
+      ]
+    },
+    { 
+      date: '03/04',
+      list: [
+        {
+          name: 'Mesada',
+          type: 'in',
+          price: 'R$ 25,99'
+        },
+        {
+          name: 'Pizzaria',
+          type: 'out',
+          price: 'R$ 45,90'
+        }
+      ]
+    },
+    { 
+      date: '04/04',
+      list: [
+        {
+          name: 'Mesada',
+          type: 'in',
+          price: 'R$ 25,99'
+        },
+        {
+          name: 'Pizzaria',
+          type: 'out',
+          price: 'R$ 45,90'
+        }
+      ]
+    },
+    { 
+      date: '05/04',
+      list: [
+        {
+          name: 'Mesada',
+          type: 'in',
+          price: 'R$ 25,99'
+        },
+        {
+          name: 'Pizzaria',
+          type: 'out',
+          price: 'R$ 45,90'
+        }
+      ]
+    },
+  ]
+
+  function handleChangeTypeList(type: string) {
+    setTypeListFilter(prev => prev === type ? '' : type)
+  }
+
   return (
-    <>Extract</>
+    <>
+      <Title>Extrato</Title>
+
+      <FilterContainer>
+        <SearchFilter />
+        <DateFilter />
+      </FilterContainer>
+
+      <ButtonContainer>
+        <ButtonFilter
+          type="button"
+          $select={typeListFilter === 'revenue'}
+          onClick={() => handleChangeTypeList('revenue')}
+        >
+          <FaArrowTrendUp size={22} fill="#0f0" />
+          Receita
+        </ButtonFilter>
+
+        <ButtonFilter 
+          type="button"
+          $select={typeListFilter === 'expense'}
+          onClick={() => handleChangeTypeList('expense')}
+        >
+          <FaArrowTrendDown size={22} fill="#f00" />
+          Despesa
+        </ButtonFilter>
+      </ButtonContainer>
+
+      <RecentExtract>
+        {extractDateList.map(({date, list}, i) => (
+          <ExtractList key={i}>
+            <ExtractItem>
+              <ExtractDate> <Line /> {date} </ExtractDate>
+              {list.map((item, index) => (
+                <ExtractItemContent key={`item-${i}-${index}`}>
+                  {item.type === 'out' 
+                    ? <FaArrowTrendDown size={18} fill="#f00" /> 
+                    : <FaArrowTrendUp size={18} fill="#0f0" />
+                  }
+                  <ExtractDesc>
+                    <span>{item.name}</span>
+                    <span>{item.price}</span>
+                  </ExtractDesc>
+                </ExtractItemContent>
+              ))}
+            </ExtractItem>
+          </ExtractList>
+        ))}
+      </RecentExtract>
+    </>
   )
 }
--- src/pages/Home/components/Extract/styles.ts
+++ src/pages/Home/components/Extract/styles.ts
@@ -1,4 +1,86 @@
 import styled from 'styled-components';
 
-export const Provisorio = styled.div`
+export const ExtractDate = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-size: 13px;
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: #888;
+`;
+
+export const ExtractItemContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-size: 13px;
+  padding: 0 4px;
+  margin-bottom: 2px;
+`;
+
+export const ExtractDesc = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const ExtractList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: 100%;
+`;
+
+export const ExtractItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const Title = styled.h2`
+  margin: 20px 20px 0 20px;
+  font-size: 22px;
+  font-weight: 400;
+`;
+
+export const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  padding: 0 10px;
+  margin: 10px 0;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 22px;
+  margin-bottom: 15px;
+`;
+
+export const ButtonFilter = styled.button<{$select?: boolean}>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+  background-color: #fff;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  outline: ${({ $select }) => $select ? 'solid 1px #1d1d1e9e' : 'none'};
+  box-shadow: 1px 2px 7px -2px #0004;
+  padding: 3px 7px;
+  min-width: 80px;
+`;
+
+export const RecentExtract = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin: 10px 20px 23px 20px;
 `;
