c7948ad60d1e938aacc1c62622bd89b66006b2c9
Jhony Freitas <jhony00._@hotmail.com>
2025-04-11 23:21:21 -0300

    feat: Create search  input

---
+++ src/components/Inputs/Search/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SearchInput = styled.input`
+  display: block;
+  font-size: 15px;
+  outline: none;
+  border-radius: 5px;    
+  box-shadow: 1px 2px 7px -2px #0004;
+  border: none;
+  margin: 0 auto;
+  padding:7px 10px;
+  width: 93%;
+`;
