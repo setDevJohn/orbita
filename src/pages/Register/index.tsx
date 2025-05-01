be0277cbc3754fa09519a2110eddf89c6838dc34
Jhony Freitas <jhony00._@hotmail.com>
2025-05-01 17:32:13 -0300

    feat: Create register componente

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,10 +1,35 @@
-import { BasicButton } from '@components/Buttons';
+import { BasicButton, ToggleButton } from '@components/Buttons';
+import { DateInput, SelectInput, TextInputWithLabel } from '@components/Inputs';
 import { LayoutContainer } from '@components/LayoutContainer';
+import { useState } from 'react';
+import { BsCreditCard } from 'react-icons/bs';
+import { FaArrowTrendDown, FaArrowTrendUp } from 'react-icons/fa6';
 import { useNavigate, useParams } from 'react-router-dom';
+import { Text } from 'styles/main';
 
-import { Content, Footer } from './styles';
+import { Content, Footer, InputContainer, Price, PriceContainer, ToggleContainer } from './styles';
+
+interface IForm {
+  description: string;
+  price: string;
+  account: string;
+  category: string;
+  date: Date | null;
+}
 
 export function Register() {
+  const formValue = {
+    description: '',
+    price: '',
+    account: '',
+    category: '',
+    date: null
+  };
+
+  const [form, setForm] = useState<IForm>(formValue);
+  const [toggleRecurrence, setToggleRecurrence] = useState<boolean>(false);
+  const [toggleMovement, setToggleMovement] = useState<boolean>(false);
+
   const { type } = useParams();
 
   const navigate = useNavigate();
@@ -14,20 +39,90 @@ export function Register() {
   const pageConfig = {
     receita: {
       title: 'Receitas',
+      icon: <FaArrowTrendUp size={30} fill="#0f0" style={{ marginRight: ' auto' }}/>
     },
     despesa: {
       title: 'Despesas',
+      icon: <FaArrowTrendDown size={30} fill="#f00" style={{ marginRight: ' auto' }}/>
     },
     'despesa-credito': {
       title: 'Despesas Crédito',
+      icon: <BsCreditCard size={30} fill="#f00" style={{ marginRight: ' auto' }}/>
     }
   }[type];
 
   if (!pageConfig) { return null; }
 
+  function handleChangeForm ( name: string, value: string | Date | null) {
+    setForm(prev => ({ ...prev, [name]: value }));    
+  }
+
   return (
     <LayoutContainer title={pageConfig.title}>
-      <Content></Content>
+      <Content>
+        <PriceContainer>
+          <Price 
+            type='text'
+            value={`R$ ${form.price || '0,00'}`}
+            onChange={({ target :{ value } }) => handleChangeForm('price', value)}
+          />
+
+          {pageConfig.icon}
+        </PriceContainer>
+
+        <InputContainer>
+          <TextInputWithLabel
+            value={form.description}
+            label='Descrição'
+            name='description'
+            handleChange={handleChangeForm}
+          />
+
+          <SelectInput
+            label='Conta'
+            name='account'
+            value={form.account}
+            placeholder='Selecione uma conta'
+            handleChange={handleChangeForm}
+            options={[]}
+          />
+
+          <SelectInput
+            label='Categoria'
+            name='category'
+            value={form.category}
+            placeholder='Selecione uma categoria'
+            handleChange={handleChangeForm}
+            options={[]}
+          />
+            
+          <DateInput 
+            startDate={form.date}
+            handleChange={(date) => handleChangeForm('date', date)}
+            placeholder='Selecione uma data'
+          />
+
+          <ToggleContainer>
+            <Text>Recorrência</Text>
+
+            {/* Ao fechar o toggle, limpar seleçoes referentes a recorrência */}
+            <ToggleButton 
+              checked={toggleRecurrence}
+              handleClick={() => setToggleRecurrence(prev => !prev)}
+            />
+          </ToggleContainer>
+
+          <ToggleContainer>
+            <Text>Movimentação entre contas</Text>
+
+            {/* Ao fechar o toggle, limpar seleçoes referentes a movimentação */}
+            <ToggleButton 
+              checked={toggleMovement}
+              handleClick={() => setToggleMovement(prev => !prev)}
+            />
+          </ToggleContainer>
+        </InputContainer>
+      </Content>
       
       <Footer>
         <BasicButton 
--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -4,7 +4,39 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-  padding: 10px;
+  padding: 30px 15px 15px 15px;
+  width: 100%;
+  overflow: auto;
+`;
+
+export const PriceContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+  width: 100%;
+`;
+
+export const Price = styled.input`
+  background-color: transparent;
+  color: ${({ theme }) => theme.textColor};
+  font-size: ${({ theme }) => theme.bigTitle};
+  border: none;
+  outline: none;
+  border-bottom: ${({ theme }) => `2px solid ${theme.color1}`};
+  padding-left: 15px;
+  margin-left: 12px;
+  margin-right: 15px;
+  width: 45%;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  gap: 15px;
+  border: ${({ theme }) => theme.color1} solid 2px;
+  border-radius: 5px;
+  padding: 15px;
   width: 100%;
   overflow: auto;
 `;
@@ -14,5 +46,13 @@ export const Footer = styled.footer`
   justify-content: space-around;
   background-color: ${({ theme }) => theme.color1};
   padding: 15px 20px;
+  width: 100%;
+`;
+
+export const ToggleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 20px;
   width: 100%;
 `;
