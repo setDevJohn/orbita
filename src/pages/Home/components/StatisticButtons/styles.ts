9e0ad2105a056802aa075bdf7331671c5ffaa9fe
Jhony Freitas <jhony00._@hotmail.com>
2025-05-01 17:32:25 -0300

    styles> Using text color context

--- src/pages/Home/components/StatisticButtons/styles.ts
+++ src/pages/Home/components/StatisticButtons/styles.ts
@@ -6,12 +6,12 @@ export const ButtonFilter = styled.button<{$select?: boolean}>`
   align-items: center;
   gap: 4px;
   background-color: ${({ theme }) => theme.color4};
-  color: #fff;
+  color: ${({ theme }) => theme.textColor};
   font-size: ${({ theme }) => theme.normalText};
   border: none;
   border-radius: 5px;
-  outline: ${({ $select }) => $select ? 'solid 1px #1d1d1e' : 'none'};
-  box-shadow: 1px 2px 7px -2px #0004;
+  outline: ${({ $select, theme }) => $select ? `solid 1px ${theme.color1}` : 'none'};
+  box-shadow: 2px 2px 7px -2px #0004;
   padding: 3px 7px;
   min-width: 80px;
 `;
