c420596a21c8aa8bffe4f811f01bd3a5ebb1c618
Jhony Freitas <jhony00._@hotmail.com>
2025-05-01 02:39:36 -0300

    feat : Add register rotue

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,17 @@
 import { Home } from '@pages/Home';
 import { NaviGation } from '@pages/Navigation';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Register } from '@pages/Register';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to='/inicio' />} />
+
         <Route path='/' element={<NaviGation />}>
-          <Route index element={<Home />} />
+          <Route index path='/inicio' element={<Home />} />
+          <Route path='/inicio/registro/:type' element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
