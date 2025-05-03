635aa76f3b752f4de89a20e6404862bcbe162b32
Jhony Freitas <jhony00._@hotmail.com>
2025-05-03 18:23:48 -0300

    fix: Fixing error with any

--- src/components/Inputs/Date/index.tsx
+++ src/components/Inputs/Date/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import 'react-datepicker/dist/react-datepicker.css';
 import { ptBR } from 'date-fns/locale';
 import DatePicker from 'react-datepicker';
@@ -28,26 +29,29 @@ export function DateInput({
     showYearPicker: mode === 'year',
   };
 
+  const dateFormat = 
+    mode === 'year'
+      ? 'yyyy'
+      : mode === 'month'
+        ? 'MM/yyyy'
+        : mode === 'week'
+          ? '\'Semana\' w'
+          : 'dd/MM/yyyy';
+
   return (
     <DatePicker
-      className="custom-date-picker"
-      selected={startDate}
-      onChange={handleChange}
-      startDate={isRange ? startDate : undefined}
-      endDate={isRange ? endDate : undefined}
-      selectsRange={isRange}
-      placeholderText={placeholder}
-      dateFormat={
-        mode === 'year'
-          ? 'yyyy'
-          : mode === 'month'
-            ? 'MM/yyyy'
-            : mode === 'week'
-              ? '\'Semana\' w'
-              : 'dd/MM/yyyy'
-      }
-      locale={ptBR}
-      {...showPickerProps}
+      {...({
+        className: 'custom-date-picker',
+        selected: startDate,
+        onChange: handleChange,
+        startDate: isRange ? startDate : undefined,
+        endDate: isRange ? endDate : undefined,
+        selectsRange: isRange ? true : undefined,
+        placeholderText: placeholder,
+        dateFormat: dateFormat,
+        locale: ptBR,
+        ...showPickerProps,
+      } as any)}
     />
   );
 }
