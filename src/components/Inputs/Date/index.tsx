cd457975ceb6448cb5eedb0b506d3c701a7544ce
Jhony Freitas <jhony00._@hotmail.com>
2025-05-03 17:51:10 -0300

    feat: Update Date component

--- src/components/Inputs/Date/index.tsx
+++ src/components/Inputs/Date/index.tsx
@@ -3,21 +3,51 @@ import { ptBR } from 'date-fns/locale';
 import DatePicker from 'react-datepicker';
 import './styles.css';
 
+export type Mode = 'day' | 'week' | 'month' | 'year';
+
 interface IDateInput {
-  startDate: Date | null
-  handleChange: (date: Date | null) => void;
+  handleChange: (date: Date | [Date | null, Date | null]) => void;
   placeholder: string;
+  startDate: Date | null;
+  endDate?: Date | null;
+  isRange?: boolean;
+  mode?: Mode;
 }
 
-export function DateInput({ startDate, handleChange, placeholder }: IDateInput) {
+export function DateInput({
+  startDate,
+  endDate,
+  handleChange,
+  placeholder,
+  isRange = false,
+  mode = 'day',
+}: IDateInput) {
+  const showPickerProps = {
+    showWeekNumbers: mode === 'week',
+    showMonthYearPicker: mode === 'month',
+    showYearPicker: mode === 'year',
+  };
+
   return (
     <DatePicker
       className="custom-date-picker"
       selected={startDate}
-      onChange={(date) => handleChange(date)}
+      onChange={handleChange}
+      startDate={isRange ? startDate : undefined}
+      endDate={isRange ? endDate : undefined}
+      selectsRange={isRange}
       placeholderText={placeholder}
-      dateFormat="dd/MM/yyyy"
+      dateFormat={
+        mode === 'year'
+          ? 'yyyy'
+          : mode === 'month'
+            ? 'MM/yyyy'
+            : mode === 'week'
+              ? '\'Semana\' w'
+              : 'dd/MM/yyyy'
+      }
       locale={ptBR}
+      {...showPickerProps}
     />
   );
 }
