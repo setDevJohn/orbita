f4c603f180dea0b8ccf65aa5dbb93925830812d0
Jhony Freitas <jhony00._@hotmail.com>
2025-05-04 14:47:45 -0300

    feat: Create conveting currency to decimal mask

--- src/utils/mask.ts
+++ src/utils/mask.ts
@@ -4,6 +4,13 @@ const currency = (value: string) => {
   return `R$ ${(+value / 100).toFixed(2).replace('.', ',')}`;
 };
 
+const currencyToDecimal = (value: string) => {
+  value = value.replace(/\D/g, '');
+
+  return Number(value) / 100;
+};
+
 export const mask = {
   currency,
+  currencyToDecimal
 };
