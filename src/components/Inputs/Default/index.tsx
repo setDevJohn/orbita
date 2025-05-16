6a59db446b8a693339a71ef741d54f22a4765beb
Jhony Freitas <jhony00._@hotmail.com>
2025-05-16 02:09:56 -0300

    styles: Add wodth

--- src/components/Inputs/Default/index.tsx
+++ src/components/Inputs/Default/index.tsx
@@ -5,16 +5,18 @@ interface ITextWithLabel {
   value: string;
   handleChange: (name: string, value: string) => void;
   type?: string
+  width?: string
   placeholder?: string;
 }
 
-export function DefaultInput({ name, value, handleChange, type, placeholder }: ITextWithLabel) {
+export function DefaultInput({ name, value, handleChange, type, width, placeholder }: ITextWithLabel) {
   return (
     <TextInput 
       id={name}
       type={type || 'text'}
       name={name}
       value={value}
+      $width={width}
       placeholder={placeholder}
       onChange={({ target :{ value, name } }) => handleChange(name, value)}
     />
--- src/components/Inputs/Default/styles.ts
+++ src/components/Inputs/Default/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const TextInput = styled.input`
+interface ITextInput {
+  $width?: string;
+}
+
+export const TextInput = styled.input<ITextInput>`
   background-color: ${({ theme }) => theme.darkBackground};
   color: ${({ theme }) => theme.textColor};
   font-size: ${({ theme }) => theme.normalText};
@@ -9,6 +13,7 @@ export const TextInput = styled.input`
   border-radius: 4px;
   box-shadow: 1px 1px 7px -1px #0009;
   padding: 10px 15px;
+  width: ${({ $width }) => $width && $width};
 
   &::placeholder {
     color: #a29f9f;
