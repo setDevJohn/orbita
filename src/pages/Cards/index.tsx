0419c43549ff43f8012f1af75c3de842d83d060d
Jhony Freitas <jhony00._@hotmail.com>
2025-05-18 21:32:06 -0300

    feat: Adding form

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,9 +1,73 @@
+import { Form } from '@components/Form';
 import { LayoutContainer } from '@components/LayoutContainer';
+import { mask } from '@utils/mask';
+import { useState } from 'react';
+
+interface IForm {
+  name: string;
+  limit: string;
+  closingDate: string;
+  dueDate: string;
+}
 
 export function Cards() {
+  const [form, setForm] = useState<IForm>({
+    name: '',
+    limit: '0',
+    closingDate: '',
+    dueDate: '',
+  });
+
+  const handleChange = (name: string, value: string) => {
+    setForm(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {};
+
   return (
     <LayoutContainer title="Cartões">
-      <div>Cartão</div>
+      <Form 
+        title="Novo Cartão"
+        onSubmit={handleSubmit}
+        inputs={[
+          {
+            type: 'default',
+            name: 'name',
+            value: form.name,
+            handleChange,
+            label: 'Nome',
+            placeholder: 'Cartão banco 1',
+            labelInColumn: true
+          },
+          {
+            type: 'default',
+            name: 'limit',
+            value: mask.currency(form.limit),
+            handleChange,
+            label: 'Limite de crédito',
+            placeholder: 'RS 945,00',
+            labelInColumn: true
+          },
+          {
+            type: 'default',
+            name: 'closingDate',
+            value: form.closingDate,
+            handleChange,
+            label: 'Dia do fechamento',
+            placeholder: '01',
+            labelInColumn: true
+          },
+          {
+            type: 'default',
+            name: 'dueDate',
+            value: form.dueDate,
+            handleChange,
+            label: 'Dia do vencimento',
+            placeholder: '05',
+            labelInColumn: true
+          },
+        ]}
+      />
     </LayoutContainer>
   );
 }
