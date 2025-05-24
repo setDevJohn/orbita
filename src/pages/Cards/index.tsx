ecc94217851c1e7861c176fb76954842dfe80820
Jhony Freitas <jhony00._@hotmail.com>
2025-05-24 01:40:13 -0300

    feat: Add form

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,7 +1,11 @@
-import { Form } from '@components/Form';
+import { CardList } from '@components/CardList';
+import { FieldsProps, Form } from '@components/Form';
 import { LayoutContainer } from '@components/LayoutContainer';
+import { LoadingPage } from '@components/Loading';
 import { mask } from '@utils/mask';
-import { useState } from 'react';
+import { toastError } from '@utils/toast';
+import { useEffect, useState } from 'react';
+import { Separator, Title } from 'styles/main';
 
 interface IForm {
   name: string;
@@ -11,12 +15,28 @@ interface IForm {
 }
 
 export function Cards() {
+  const [loading, setLoading] = useState(true);
+  const [cardList, setCardList] = useState([]);
   const [form, setForm] = useState<IForm>({
     name: '',
-    limit: '0',
+    limit: '',
     closingDate: '',
     dueDate: '',
   });
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+      } catch (err) {
+        toastError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleChange = (name: string, value: string) => {
     setForm(prev => ({ ...prev, [name]: value }));
@@ -24,50 +44,61 @@ export function Cards() {
 
   const handleSubmit = () => {};
 
+  const fieldsForm: FieldsProps[] = [
+    {
+      type: 'default',
+      name: 'name',
+      value: form.name,
+      handleChange,
+      label: 'Nome',
+      placeholder: 'Cartão banco 1',
+      labelInColumn: true
+    },
+    {
+      type: 'default',
+      name: 'limit',
+      value: mask.currency(form.limit),
+      handleChange,
+      label: 'Limite de crédito',
+      labelInColumn: true
+    },
+    {
+      type: 'default',
+      name: 'closingDate',
+      value: form.closingDate,
+      handleChange,
+      label: 'Dia do fechamento',
+      placeholder: '01',
+      labelInColumn: true
+    },
+    {
+      type: 'default',
+      name: 'dueDate',
+      value: form.dueDate,
+      handleChange,
+      label: 'Dia do vencimento',
+      placeholder: '05',
+      labelInColumn: true
+    },
+  ];
+
   return (
     <LayoutContainer title="Cartões">
       <Form 
         title="Novo Cartão"
         onSubmit={handleSubmit}
-        inputs={[
-          {
-            type: 'default',
-            name: 'name',
-            value: form.name,
-            handleChange,
-            label: 'Nome',
-            placeholder: 'Cartão banco 1',
-            labelInColumn: true
-          },
-          {
-            type: 'default',
-            name: 'limit',
-            value: mask.currency(form.limit),
-            handleChange,
-            label: 'Limite de crédito',
-            placeholder: 'RS 945,00',
-            labelInColumn: true
-          },
-          {
-            type: 'default',
-            name: 'closingDate',
-            value: form.closingDate,
-            handleChange,
-            label: 'Dia do fechamento',
-            placeholder: '01',
-            labelInColumn: true
-          },
-          {
-            type: 'default',
-            name: 'dueDate',
-            value: form.dueDate,
-            handleChange,
-            label: 'Dia do vencimento',
-            placeholder: '05',
-            labelInColumn: true
-          },
-        ]}
+        fields={fieldsForm}
       />
+
+      <Separator $margin={20}/>
+
+      <Title $margin='20px 0 0 0'>
+        {!cardList.length ? 'Nenhum cartão cadastrado' : 'Cartões cadastrados'}
+      </Title>
+
+      <CardList />
+
+      {loading && <LoadingPage />}
     </LayoutContainer>
   );
 }
