a6e76e04570c1f44cf46efe3e3833be428e32208
Jhony Freitas <jhony00._@hotmail.com>
2025-05-24 01:37:44 -0300

    feat: Add form

--- src/pages/Accounts/index.tsx
+++ src/pages/Accounts/index.tsx
@@ -1,9 +1,71 @@
+import { FieldsProps, Form } from '@components/Form';
 import { LayoutContainer } from '@components/LayoutContainer';
+import { LoadingPage } from '@components/Loading/Page';
+import { mask } from '@utils/mask';
+import { toastError } from '@utils/toast';
+import { useEffect, useState } from 'react';
+
+interface IForm {
+  name: string;
+  balance: string;
+}
 
 export function Accounts() {
+  const [loading, setLoading] = useState(true);
+  const [form, setForm] = useState<IForm>({
+    name: '',
+    balance: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+      } catch (err) {
+        toastError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleChange = (name: string, value: string) => {
+    setForm(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {};
+
+  const fieldsForm: FieldsProps[] = [
+    {
+      type: 'default',
+      name: 'name',
+      value: form.name,
+      handleChange,
+      label: 'Nome',
+      placeholder: 'Conta Poupanca',
+      labelInColumn: true
+    },
+    {
+      type: 'default',
+      name: 'balance',
+      value: mask.currency(form.balance),
+      handleChange,
+      label: 'Saldo',
+      labelInColumn: true
+    },
+  ];
+
   return (
     <LayoutContainer title="Contas">
-      <div>Conta</div>
+      <Form 
+        title='Nova Conta'
+        onSubmit={handleSubmit}
+        fields={fieldsForm}
+      />
+
+      {loading && <LoadingPage />}
     </LayoutContainer>
   );
 }
--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,9 +1,62 @@
+import { FieldsProps, Form } from '@components/Form';
 import { LayoutContainer } from '@components/LayoutContainer';
+import { LoadingPage } from '@components/Loading';
+import { toastError } from '@utils/toast';
+import { useEffect, useState } from 'react';
+
+interface IForm {
+  name: string;
+  balance: string;
+}
 
 export function Categories() {
+  const [loading, setLoading] = useState(true);
+  const [form, setForm] = useState<IForm>({
+    name: '',
+    balance: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+      } catch (err) {
+        toastError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+  const handleChange = (name: string, value: string) => {
+    setForm(prev => ({ ...prev, [name]: value }));
+  };
+  
+  const handleSubmit = () => {};
+
+  const fieldsForm: FieldsProps[] = [
+    {
+      type: 'default',
+      name: 'name',
+      value: form.name,
+      handleChange,
+      label: 'Nome',
+      placeholder: 'Comida',
+      labelInColumn: true
+    }
+  ];
+
   return (
     <LayoutContainer title="Categorias">
-      <div>Categorias</div>
+      <Form
+        title='Nova Categoria'
+        onSubmit={handleSubmit}
+        fields={fieldsForm}
+      />
+      
+      {loading && <LoadingPage />}
     </LayoutContainer>
   );
 }
