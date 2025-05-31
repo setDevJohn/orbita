a21adf4b9f6f07645d6cdcfba2952a032eeaf481
Jhony Freitas <jhony00._@hotmail.com>
2025-05-31 19:54:02 -0300

    feat: Update card

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,15 +1,18 @@
+import { BasicButton } from '@components/Buttons';
 import { CardList } from '@components/CardList';
 import { FieldsProps, Form } from '@components/Form';
 import { LayoutContainer } from '@components/LayoutContainer';
 import { LoadingPage } from '@components/Loading';
 import { cardsApi } from '@services/cards';
+import { CardRaw } from '@services/cards/interface';
 import { format } from '@utils/format';
 import { mask } from '@utils/mask';
 import { toastError, toastSuccess, toastWarn } from '@utils/toast';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Separator, Title } from 'styles/main';
 
 interface IForm {
+  id?: number;
   name: string;
   creditLimit: string;
   closingDay: string;
@@ -18,20 +21,26 @@ interface IForm {
 
 export function Cards() {
   const [loading, setLoading] = useState(true);
-  const [cardList, setCardList] = useState([]);
+  const [cardList, setCardList] = useState<CardRaw[]>([]);
   const [reloadList, setReloadList] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [openForm, setOpenForm] = useState(false);
   const [form, setForm] = useState<IForm>({
     name: '',
     creditLimit: '',
     closingDay: '',
     dueDay: '',
   });
+
+  const contentRef = useRef<HTMLDivElement | null>(null);
   
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        setCardList([]);
+
+        const response = await cardsApi.get();
+        setCardList(response);
       } catch (err) {
         toastError((err as Error).message);
       } finally {
@@ -48,8 +57,12 @@ export function Cards() {
 
   const reloadComponent = () => {
     setReloadList(!reloadList);
+  };
+
+  const handleCancel = () => {
     setForm({ name: '', creditLimit: '', closingDay: '', dueDay: '' });
-    // TODO tirar o foco do input
+    setEditMode(false);
+    setOpenForm(false);
   };
 
   const handleSubmit = async () => {
@@ -68,15 +81,39 @@ export function Cards() {
         dueDay: Number(form.dueDay),
       };
 
-      await cardsApi.create(payload);
+      if (editMode) {
+        if (!form.id) { 
+          return toastError('Erro ao atualizar o cartão.');
+        }
+
+        await cardsApi.update({ ...payload, id: form.id });
+        toastSuccess('Cartão atualizado com sucesso.');
+        setEditMode(false);
+      } else {
+        await cardsApi.create(payload);
+        toastSuccess('Cartão cadastrado com sucesso.');
+      }
       
-      toastSuccess('Cartão cadastrado com sucesso.');
+      handleCancel();
       reloadComponent();
     } catch (err) {
       toastError((err as Error).message);
     } finally {
       setLoading(false);
     }
+  };
+
+  const handleEdit = (card: CardRaw) => {
+    setForm({
+      id: card.id,
+      name: card.name,
+      creditLimit: mask.brlCurrency(card.creditLimit.toString()),
+      closingDay: String(card.closingDay).padStart(2, '0'),
+      dueDay: String(card.dueDay).padStart(2, '0'),
+    });
+    setEditMode(true);
+    setOpenForm(true);
+    contentRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   const fieldsForm: FieldsProps[] = [
@@ -117,13 +154,31 @@ export function Cards() {
     },
   ];
 
+  const formProps = {
+    onSubmit: handleSubmit,
+    fields: fieldsForm,
+    cancelFunction: handleCancel
+  };
+
   return (
-    <LayoutContainer title="Cartões">
-      <Form 
-        title="Novo Cartão"
-        onSubmit={handleSubmit}
-        fields={fieldsForm}
-      />
+    <LayoutContainer title="Cartões" ref={contentRef}>
+      
+      {openForm 
+        ? (
+          <>
+            {editMode 
+              ? <Form title="Editar Cartão" {...formProps} />
+              : <Form title="Novo Cartão" {...formProps} />
+            }
+          </>
+        ) : (
+          <BasicButton
+            text="Novo Cartão"
+            action={() => setOpenForm(true)}
+            custonStyle={{ width: '70%', marginTop: '20px' }}
+          />
+        )
+      }
 
       <Separator $margin={20}/>
 
@@ -131,7 +186,11 @@ export function Cards() {
         {!cardList.length ? 'Nenhum cartão cadastrado' : 'Cartões cadastrados'}
       </Title>
 
-      <CardList />
+      <CardList 
+        editMode 
+        cardList={cardList}
+        editFunction={handleEdit}
+      />
 
       {loading && <LoadingPage />}
     </LayoutContainer>
