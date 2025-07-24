d2e6b135ae2316548b09e7131f9c305c4662c0d6
Jhony Freitas <jhony00._@hotmail.com>
2025-07-23 23:57:33 -0300

    feat: Create size prop to icon

--- src/components/TransactionIcon/index.tsx
+++ src/components/TransactionIcon/index.tsx
@@ -2,12 +2,19 @@ import { FaArrowTrendDown, FaArrowTrendUp } from 'react-icons/fa6';
 
 import { IconContainer } from './styles';
 
-export const TransactionIcon = ({ type }: {type: 'income' | 'expense'}) => {
+interface TransactionIconProps {
+  type: 'income' | 'expense';
+  size?: 'small' | 'medium';
+}
+
+export const TransactionIcon = ({ type, size = 'medium' }: TransactionIconProps) => {
+  const iconSize = size === 'small' ? 18 : 22;
+
   return (
-    <IconContainer $operationType={type}>
+    <IconContainer $operationType={type} $size={size}>
       {type === 'income' 
-        ? <FaArrowTrendUp size={22} fill="#22c55e" /> 
-        : <FaArrowTrendDown size={22} fill="#ef4444" />
+        ? <FaArrowTrendUp size={iconSize} fill="#22c55e" /> 
+        : <FaArrowTrendDown size={iconSize} fill="#ef4444" />
       }
     </IconContainer>
   );
--- src/components/TransactionIcon/styles.ts
+++ src/components/TransactionIcon/styles.ts
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
-export const IconContainer = styled.div<{$operationType: 'income' | 'expense'}>`
+export const IconContainer = styled.div<{
+  $operationType: 'income' | 'expense';
+  $size: 'small' | 'medium'
+}>`
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: ${({ $operationType }) => $operationType === 'income' ? '#22c55e33' : '#ef444433' };
   border-radius: 50%;
-  width: 48px;
-  height: 48px;
+  width: ${({ $size }) => $size === 'small' ? '28px' : '42px'};
+  height: ${({ $size }) => $size === 'small' ? '28px' : '42px'};
 `;
