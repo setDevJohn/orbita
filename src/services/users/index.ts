ea7ce6dc870459d4ea561a626cb69a2eb2d77af2
john <jhony00._@hotmail.com>
2025-07-30 14:26:19 -0300

    feat: Create users services

---
+++ src/services/users/index.ts
@@ -0,0 +1,27 @@
+import { api } from '@services/api';
+import { handlerAxiosError } from '@utils/axiosError';
+
+import { UserFormLogin, UserFormPayload } from './interface';
+
+async function login (data: UserFormLogin) {
+  try {
+    const { data: response } = await api.post('/users/auth', data);
+    return response.resource;
+  } catch (err) {
+    handlerAxiosError(err);
+  }
+}
+
+async function register (data: UserFormPayload) {
+  try {
+    const { data: response } = await api.post('/accounts', data);
+    return response.resource;
+  } catch (err) {
+    handlerAxiosError(err);
+  }
+}
+
+export const usersApi = {
+  login,
+  register,
+};
