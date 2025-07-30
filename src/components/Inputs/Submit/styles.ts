8abf977e28b25860a9f7b4c17b8c6f8ff4c8fcc1
Jhony Freitas <jhony00._@hotmail.com>
2025-07-29 21:37:17 -0300

    feat: Create submit componente

---
+++ src/components/Inputs/Submit/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const SubmitStyled = styled.input`
+  background: ${({ theme }) => theme.linearGradient};
+  color: #fff;
+  font-size: ${({ theme }) => theme.normalText};
+  font-family: ${({ theme }) => theme.primaryFont};
+  font-weight: 600;
+  border-radius: 10px;
+  padding: 15px;
+  margin: 10px 0;
+  border: none;
+  outline: none;
+`;
