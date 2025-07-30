f0a4d26edac316ae8e177324a585275279cdff05
john <jhony00._@hotmail.com>
2025-07-30 10:14:22 -0300

    merge remote feat/login into local login

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const apiBaseUrl = import.meta.env.VITE_API_URL; 
 
+if (!apiBaseUrl) {
+  throw new Error('Variável de ambiente VITE_API_URL não encontrada!');
+}
+
 export const api = axios.create({ baseURL: apiBaseUrl });
 
 api.interceptors.request.use((config) => {
