c4aae8056b6ab001c36ab1f2068d3effb6897328
john <jhony00._@hotmail.com>
2025-08-04 11:22:36 -0300

    feat: Allow new origin

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   preview: {
-    allowedHosts: ['wallet.johndevstudio.com'],
+    allowedHosts: ['orbita.johndevstudio.com'],
   },	
   resolve: {
     alias: {
