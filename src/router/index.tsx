e23bc653a924c1fc6907c713c357617dab3365c0
john <jhony00._@hotmail.com>
2025-08-06 16:06:36 -0300

    feat: Add register route and reset password route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,12 +2,12 @@ import { Accounts } from '@pages/Accounts';
 import { Cards } from '@pages/Cards';
 import { Categories } from '@pages/Categories';
 import { Home } from '@pages/Home';
-import { Login } from '@pages/Login';
 import { NaviGation } from '@pages/Navigation';
 import { Notifications } from '@pages/Notifications';
 import { Profile } from '@pages/Profile';
 import { Register } from '@pages/Register';
 import { Settings } from '@pages/Settings';
+import { Login, RegisterUser, ResetPassword } from '@pages/Users';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 export const AppRoutes = () => {
@@ -17,6 +17,8 @@ export const AppRoutes = () => {
         <Route path='/' element={<Navigate to='/inicio' />} />
 
         <Route path='/login' element={<Login />} />
+        <Route path='/registrar' element={<RegisterUser />} />
+        <Route path='/recuperar-senha' element={<ResetPassword />} />
 
         <Route path='/' element={<NaviGation />}>
           <Route index path='/inicio' element={<Home />} />
