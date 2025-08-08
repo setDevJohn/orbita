74e2497dead9c3970e94634430e664310e78a975
john <jhony00._@hotmail.com>
2025-08-08 14:52:38 -0300

    feat: Create public and privates routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,35 +2,80 @@ import { Accounts } from '@pages/Accounts';
 import { Cards } from '@pages/Cards';
 import { Categories } from '@pages/Categories';
 import { Home } from '@pages/Home';
-import { NaviGation } from '@pages/Navigation';
+import { Navigation } from '@pages/Navigation';
 import { Notifications } from '@pages/Notifications';
 import { Profile } from '@pages/Profile';
 import { Register } from '@pages/Register';
 import { Settings } from '@pages/Settings';
 import { Login, RegisterUser, ResetPassword } from '@pages/Users';
+import { useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 export const AppRoutes = () => {
+  const [authencticated, setAuthencticated] = useState(true);
+
+  const routes = {
+    publics: [
+      { path: '/login', element: <Login /> },
+      { path: '/registrar', element: <RegisterUser /> },
+      { path: '/recuperar-senha', element: <ResetPassword /> },
+    ],
+    privates: [
+      { path: '/inicio', element: <Home /> },
+      { path: '/inicio/registro/:type', element: <Register /> },
+      { path: '/perfil', element: <Profile /> },
+      { path: '/contas', element: <Accounts /> },
+      { path: '/cartoes', element: <Cards /> },
+      { path: '/categorias', element: <Categories /> },
+      { path: '/notificacoes', element: <Notifications /> },
+      { path: '/configuracoes' , element: <Settings /> },
+    ]
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Navigate to='/inicio' />} />
+        { !authencticated ? (
+          <>
+            <Route path='/' element={ <Navigate to='/login' /> } />
 
-        <Route path='/login' element={<Login />} />
-        <Route path='/registrar' element={<RegisterUser />} />
-        <Route path='/recuperar-senha' element={<ResetPassword />} />
+            {routes.publics.map(({ path, element }, i) => (
+              <Route key={i} path={ path } element={ element } />
+            ))}
 
-        <Route path='/' element={<NaviGation />}>
-          <Route index path='/inicio' element={<Home />} />
-          <Route path='/inicio/registro/:type' element={<Register />} />
-          <Route path='/perfil' element={<Profile /> } />
-          <Route path='/contas' element={<Accounts /> } />
-          <Route path='/cartoes' element={<Cards /> } />
-          <Route path='/categorias' element={<Categories /> } />
-          <Route path='/notificacoes' element={<Notifications /> } />
-          <Route path='/configuracoes' element={<Settings /> } />
-        </Route>
+            <Route path='*' element={ <Navigate to='/login' /> } />
+          </>
+        ) : (
+          <>
+            <Route path='/' element={ <Navigate to='/inicio' /> } />
+
+            <Route path='/' element={ <Navigation /> }>
+              {routes.privates.map(({ path, element }, i) => (
+                <Route key={i} index={ i === 0 } path={ path } element={ element } />
+              ))}
+            </Route>
+
+            <Route path='*' element={ <Navigate to='/inicio' /> } />
+          </>
+        ) }
       </Routes>
     </BrowserRouter>
   );
 };
+
+//  <Route path='/' element={<Navigate to='/inicio' />} />
+
+//   <Route path='/login' element={<Login />} />
+//   <Route path='/registrar' element={<RegisterUser />} />
+//   <Route path='/recuperar-senha' element={<ResetPassword />} />
+
+//   <Route path='/' element={<NaviGation />}>
+//     <Route index path='/inicio' element={<Home />} />
+//     <Route path='/inicio/registro/:type' element={<Register />} />
+//     <Route path='/perfil' element={<Profile /> } />
+//     <Route path='/contas' element={<Accounts /> } />
+//     <Route path='/cartoes' element={<Cards /> } />
+//     <Route path='/categorias' element={<Categories /> } />
+//     <Route path='/notificacoes' element={<Notifications /> } />
+//     <Route path='/configuracoes' element={<Settings /> } />
+//   </Route>
