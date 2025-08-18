e6b63ce8461485212dced80a2a22c7395b7680c2
john <jhony00._@hotmail.com>
2025-08-18 16:53:31 -0300

    feat: Separate page in three steps

---
+++ src/pages/Users/ResetPassword/index.ts
@@ -0,0 +1,5 @@
+import { EmailStep } from './EmailStep';
+import { NewPasswordStep } from './NewPaswordStep';
+import { TokenStep } from './TokenStep';
+
+export { EmailStep, TokenStep, NewPasswordStep };
