bde01aa32340698cd7c7594698c4f1064e28a8b5
john <jhony00._@hotmail.com>
2025-08-19 11:22:18 -0300

    fix: FIxing import

--- src/pages/Users/index.ts
+++ src/pages/Users/index.ts
@@ -1,5 +1,4 @@
 import { Login } from './Login';
 import { RegisterUser } from './RegisterUser';
-import { ResetPassword } from './ResetPassword';
 
-export { Login, RegisterUser, ResetPassword };
+export { Login, RegisterUser };
